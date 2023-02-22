856f3906ceb4e342387cfd6a61ebea75cd7d5632
“Nicole <nicole@agentur-pottkinder.de>
2023-02-22 10:43:52 +0100

    started python version of Downloader

---
+++ CSVDownloader_Python/CSVDownloader.py
@@ -0,0 +1,12 @@
+import datetime
+
+class CSVDownloader:
+    def __init__(fileName):
+        self.sensorURL = "http://archive.sensor.community/"
+        self.startDate = datetime.datetime(2022, 1, 1)
+        self.endDate = datetime.datetime(2023, 2, 21)
+        self.saveFile = fileName
+
+    def main():
+
+    def getSensorName():
